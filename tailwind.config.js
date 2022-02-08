5d86f2c894190389f8daf6efc13a65e42f2adace
mancfactor <frankmancina@gmail.com>
2022-02-08 17:24:10 -0500

    working blog

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./components/**/*.{html,js}","./pages/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ["./components/**/*.{html,js}","./pages/**/*.{html,js}"]
+}
